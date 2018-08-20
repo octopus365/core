f73b70ef81e4cc32614150ee3e75451c6c1c3bb6
uebtyo <octopus365@gmail.com>
2018-08-20 22:58:59 +0900

    record link nums

--- make_listcsv.py
+++ make_listcsv.py
@@ -7,6 +7,7 @@ import pandas as pd
 import email
 import zipfile
 import datetime
+import pickle
 
 from modules.config import parent_dir_path
 from modules.config import save_dir_path
@@ -38,6 +39,11 @@ if __name__ == "__main__":
     dir_name_list.remove("Tech")  # Tech has sub folders
     dir_name_list.remove("Column")
     dir_name_list.remove("__forAirg")
+    if os.path.exists(save_dir_path + "\\tmp_csv\\link_num_dic.pickle"):
+        with open(save_dir_path + "\\tmp_csv\\link_num_dic.pickle", mode='rb') as f:
+            link_num_dic = pickle.load(f)
+    else:
+        link_num_dic = {}
 
     tech_dir_list = os.listdir(parent_dir_path + "\\Tech")
     for tech_dir_tmp in tech_dir_list:
@@ -51,17 +57,26 @@ if __name__ == "__main__":
         current_dir_path = parent_dir_path + "\\" + target_dir_tmp
         mhtml_list = os.listdir(current_dir_path)
         csv_df = from_mhtml_to_df(mhtml_list, current_dir_path)
-
-        if target_dir_tmp[0:4]=="Tech":
-            save_file_name = save_dir_path + "\\tmp_csv\\Tech_" + target_dir_tmp[5:] + ".csv"
-        elif target_dir_tmp[0:6] == "Column":
-            save_file_name = save_dir_path + "\\tmp_csv\\Column_" + target_dir_tmp[7:] + ".csv"
+        if (target_dir_tmp in link_num_dic):
+            past_link_num = link_num_dic[target_dir_tmp]
         else:
-            save_file_name = save_dir_path + "\\tmp_csv\\" + target_dir_tmp + ".csv"
+            past_link_num = 0
 
-        csv_df.to_csv(save_file_name, index=False, encoding="utf-8")
+        if len(csv_df.index)>past_link_num:
+            if target_dir_tmp[0:4] == "Tech":
+                save_file_name = save_dir_path + "\\tmp_csv\\Tech_" + target_dir_tmp[5:] + ".csv"
+            elif target_dir_tmp[0:6] == "Column":
+                save_file_name = save_dir_path + "\\tmp_csv\\Column_" + target_dir_tmp[7:] + ".csv"
+            else:
+                save_file_name = save_dir_path + "\\tmp_csv\\" + target_dir_tmp + ".csv"
 
-        print(target_dir_tmp + " is finished, {}/{}".format(dir_i+1, len(dir_name_list)))
+            csv_df.to_csv(save_file_name, index=False, encoding="utf-8")
+            link_num_dic[target_dir_tmp] = len(csv_df.index)
+
+            print(target_dir_tmp + " is created, {}/{}".format(dir_i + 1, len(dir_name_list)))
+        else:
+            print(target_dir_tmp + " is passed, {}/{}".format(dir_i + 1, len(dir_name_list)))
+
 
     # make zip
     today_date = datetime.date.today()
@@ -70,5 +85,9 @@ if __name__ == "__main__":
         ziped_file_list = os.listdir(save_dir_path + "\\tmp_csv")
         for zip_tmp in ziped_file_list:
             new_zip.write(save_dir_path + "\\tmp_csv\\" + zip_tmp)
+
+    # save dic
+    with open(save_dir_path + "\\tmp_csv\\link_num_dic.pickle", mode='wb') as f:
+        pickle.dump(link_num_dic, f)
 
     print("all work has been finished")
---
+++ modules/config.py
@@ -0,0 +1,2 @@
+parent_dir_path = "E:\\Dropbox\\Saved web pages"
+save_dir_path = parent_dir_path + "\\__forAirg"
